28a6e36875884bfd7ebec96f06aa0d2d840c266c
Martin <martin.jablecnik@email.cz>
2018-10-18 21:08:03 +0200

    fixed highscore

--- Highscore.gd
+++ Highscore.gd
@@ -1,14 +1,29 @@
 extends Control
 
-var names_array = ["Monika", "Pavel", "Petr", "Tomas", "Honza", "Lukas"]
-var score_array = [1000, 2000, 5000, 10000, 20000, 30000]
+var names_array = ["Monika", "Pavel", "Petr", "Tomas", "Honza", "Lukas", "Monika", "Pavel", "Petr", "Tomas", "Honza", "Lukas"]
+var score_array = [1000, 2000, 5000, 10000, 20000, 30000, 1000, 2000, 5000, 10000, 20000, 30000]
 
+var subLabel = load("res://SubLabel.tscn")
 
 func _ready():
-	var namesContainer = $VBoxContainer/HighscoreScroll/VBoxContainer/Names
-	for name in names_array:
-		var label = Label.new()
-		label.text = name
+	var positionsContainer = $VBoxContainer/HighscoreScroll/HBoxContainer/Positions
+	var namesContainer = $VBoxContainer/HighscoreScroll/HBoxContainer/Names
+	var scoresContainer = $VBoxContainer/HighscoreScroll/HBoxContainer/Scores
+	
+	for i in names_array.size():
+		var position = subLabel.instance()
+		position.text = str(i+1)
+		
+		var label = subLabel.instance()
+		label.text = names_array[i]
+		
+		var score = subLabel.instance()
+		score.text = str(score_array[i])
+		
+		
+		positionsContainer.add_child(position) 
 		namesContainer.add_child(label) 
+		scoresContainer.add_child(score) 
+		
 	
 
